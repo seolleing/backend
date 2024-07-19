f97f0e69289cfd686245eaebfe9f1e2446a2b373
rosoa0475 <rosoa0475@gmail.com>
2024-07-19 23:56:56 +0900

    [FEAT] 프로젝트 생성

---
+++ src/main/java/com/out4ider/selleing_backend/SelleingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.out4ider.selleing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelleingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelleingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/out4ider/selleing_backend/SelleingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.out4ider.selleing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelleingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
