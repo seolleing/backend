3c9ea280da3b0201dff60f15203359f545361559
choi <rosoa0475@gmail.com>
2024-09-04 14:40:32 +0900

    [FEAT] remove not used method in ResponseDto

--- src/main/java/com/out4ider/selleing_backend/global/common/dto/ResponseDto.java
+++ src/main/java/com/out4ider/selleing_backend/global/common/dto/ResponseDto.java
@@ -1,24 +1,12 @@
 package com.out4ider.selleing_backend.global.common.dto;
 
 import com.out4ider.selleing_backend.global.exception.ExceptionBody;
-import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 
 public class ResponseDto {
     public static ResponseEntity<?> onSuccess() {
         return ResponseEntity.ok().build();
-    }
-
-    public static ResponseEntity<?> onSuccess(HttpHeaders headers) {
-        return ResponseEntity.ok().headers(headers).build();
-    }
-
-    public static <T> ResponseEntity<T> onSuccess(HttpHeaders headers, T body) {
-        return ResponseEntity.ok().headers(headers).body(body);
     }
 
     public static <T> ResponseEntity<T> onSuccess(T body) {
@@ -30,18 +18,4 @@ public class ResponseDto {
         return ResponseEntity.status(httpStatusCode).body(body);
     }
 
-    public static HttpHeaders createContentDispositionHeader(String filename) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_DISPOSITION
-                , "inline; filename=\"" + filename + "\"");
-        return headers;
-    }
-
-    public static HttpHeaders createHeaders(List<Pair<String, String>> pairs) {
-        HttpHeaders headers = new HttpHeaders();
-        for (Pair<String, String> pair : pairs) {
-            headers.add(pair.getLeft(), pair.getRight());
-        }
-        return headers;
-    }
 }
