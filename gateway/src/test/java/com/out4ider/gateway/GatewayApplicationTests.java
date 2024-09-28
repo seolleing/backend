1722d00284fa8a0748f5690f6f61bcec8d66b527
choi <rosoa0475@gmail.com>
2024-09-28 17:18:49 +0900

    [FEAT] set gateway server

---
+++ gateway/src/test/java/com/out4ider/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.out4ider.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
