171ff45cfc6cbd84402a90a4e44a92ae79e4d901
choi <rosoa0475@gmail.com>
2024-09-28 15:40:54 +0900

    [FIX] lamda expression in SecurityConfig

--- configserver/src/main/java/com/out4ider/configserver/config/SecurityConfig.java
+++ configserver/src/main/java/com/out4ider/configserver/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,7 +25,7 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((auth)->auth.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests((auth)->auth
                         .anyRequest().authenticated());
